7358a72d325118049d242f7eca58e6b0565617ce
DinoFung <dino_f@live.cn>
2021-04-04 14:27:21 +0800

    add quasar

---
+++ src/components/pictureStage.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {
+  name: 'PictureStage',
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
